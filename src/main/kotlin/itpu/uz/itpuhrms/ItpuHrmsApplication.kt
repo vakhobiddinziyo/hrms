d902722bdbc463b7da5297168dacd1926027a4ff
vakhobiddin <vakhobiddinziyobobokulov@gmail.com>
2025-03-17 10:30:12 +0500

    Initial commit

---
+++ src/main/kotlin/itpu/uz/itpuhrms/ItpuHrmsApplication.kt
@@ -0,0 +1,11 @@
+package itpu.uz.itpuhrms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItpuHrmsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItpuHrmsApplication>(*args)
+}
---
+++ src/test/kotlin/itpu/uz/itpuhrms/ItpuHrmsApplicationTests.kt
@@ -0,0 +1,13 @@
+package itpu.uz.itpuhrms
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItpuHrmsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
